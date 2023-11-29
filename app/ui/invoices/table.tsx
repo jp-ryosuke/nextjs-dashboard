9c6fe48a0bd52f304d6384afc9e54e408e0538b3
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-11-29 14:59:30 +0900

    請求書更新ページの追加

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,6 +11,7 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
+  
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
@@ -112,6 +113,10 @@ export default async function InvoicesTable({
                       <UpdateInvoice id={invoice.id} />
                       <DeleteInvoice id={invoice.id} />
                     </div>
+                  </td>
+                  <td className="flex justify-end gap-2 whitespace-nowrap px-6 py-4 text-sm">
+                    <UpdateInvoice id={invoice.id} />
+                    <DeleteInvoice id={invoice.id} />
                   </td>
                 </tr>
               ))}
