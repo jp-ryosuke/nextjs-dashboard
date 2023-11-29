9fae7c47e7bc366a51cbb15c70ab6c911c997f0e
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-11-29 13:37:58 +0900

    サーバーアクションとフォームを追加

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,11 @@
 'use server'; //ファイル内の全てのエクスポートされた関数をサーバ関数としてマークする
 
-export async function createInvoice(formData: FormData) {}
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    };
+    // Test it out:
+    console.log(rawFormData);
+  }
