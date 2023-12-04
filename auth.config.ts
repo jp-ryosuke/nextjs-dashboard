e3542335317a6f88b48dba9b987ccb7ceaea01a6
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-12-04 15:51:54 +0900

    update

--- auth.config.ts
+++ auth.config.ts
@@ -11,13 +11,16 @@ export const authConfig = {
       if (isOnDashboard) {
         if (isLoggedIn){
             console.log('ログイン済み');
+            console.log(auth?.user);
             return true;
         }else{
             console.log('Redirect unauthenticated users to login page');
+            console.log(auth?.user);
             return false;
         }
       } else if (isLoggedIn) {
         console.log('ログイン済みなので dashboard へのリダイレクトを許可する');
+        console.log(auth?.user);
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       console.log('未ログイン')
